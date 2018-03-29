11d5101a81feadc702d44a8046eaf53e02526025
Steven Lawson <sjlawson@gmail.com>
2018-03-29 14:59:35 -0700

    Forgot to validate the favorite.

--- src/public/signup/signup.controller.js
+++ src/public/signup/signup.controller.js
@@ -4,16 +4,24 @@
 angular.module('public')
 .controller('SignupController', SignupController);
 
-SignupController.$inject = ['SignupService',]
-function SignupController(SignupService) {
+SignupController.$inject = ['SignupService','MenuService']
+function SignupController(SignupService,MenuService) {
   var $ctrl = this;
   $ctrl.user = {};
   $ctrl.submit = function () {
-    $ctrl.completed = true;
-    console.log("submitted in ctrl");
-    console.log($ctrl.user);
-    SignupService.setUser($ctrl.user);
+  	MenuService.getItem($ctrl.user.favorite).then(function(result){
+  		$ctrl.completed = true;
+  		$ctrl.invalidFav = false;
+	    console.log("submitted in ctrl");
+	    console.log($ctrl.user);
+	    SignupService.setUser($ctrl.user);
+  	}).catch(function(){
+  		$ctrl.invalidFav = true;
+  	});
+    
   };
+
+  
 
 }
 
